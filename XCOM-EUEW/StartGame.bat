f7e2a44edb90805d8d23addfedd6a312543245f9
TKBS <mrrobertslm01@gmail.com>
2024-06-01 23:45:46 +0100

    Create StartGame.bat

---
+++ XCOM-EUEW/StartGame.bat
@@ -0,0 +1,1 @@
+ "M:\SteamLibrary\steamapps\common\XCom-Enemy-Unknown\Binaries\Win32\XComGame.exe" -debug -review -nosteam -nohomedir -noRedScreens -NoLoadingScreen -allowconsole=true -cheats -physx -log
