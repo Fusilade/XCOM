e3a701bb834eb00031d890ee29a654cae8620aa3
TKBS <mrrobertslm01@gmail.com>
2024-06-04 22:44:34 +0100

    Update StartGame.bat

--- XCOM-EUEW/StartGame.bat
+++ XCOM-EUEW/StartGame.bat
@@ -1,1 +1,1 @@
- "M:\SteamLibrary\steamapps\common\XCom-Enemy-Unknown\Binaries\Win32\XComGame.exe" -debug -review -nosteam -nohomedir -noRedScreens -NoLoadingScreen -allowconsole=true -cheats -physx -log
+ "M:\SteamLibrary\steamapps\common\XCom-Enemy-Unknown\Binaries\Win32\XComGame.exe" -debug -review -nosteam -nohomedir -noRedScreens -NoLoadingScreen -allowconsole=true -cheats -physx -dx11 -log
